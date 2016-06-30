2ee78a62dbf0701996d2cc45c9915f6df5a0b04d
joshlove <josh.love@verizon.net>
2016-06-30 02:07:21 -0400

    Changes the key location for a simple vagrant box

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,6 +1,8 @@
 server 'localhost', user: 'vagrant', roles: %w{app db web}
 set :ssh_options, {
   forward_agent: true,
+  keys: %w(~/code/vagrant/.vagrant/machines/default/virtualbox/private_key),
+  auth_methods: %w(publickey),
   port: 2222
 }
  
